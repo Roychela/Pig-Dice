fb91792ddc4477c988791fc82191c0db426a4242
Roy Chela <roychela@gmail.com>
2019-05-13 00:01:55 +0300

    Add turn score functionality that adds each die number rolled

--- js/scripts.js
+++ js/scripts.js
@@ -10,12 +10,25 @@ function genRandom() {
    randomNumber=Math.floor(Math.random()*6) +1;
   return randomNumber;
  }
+ Player.prototype.rolledPlay = function() {
+  var rollDice = genRandom();
+  if(rollDice === 1){
+    this.turnScore = 0;
+    alert("Oops you rolled a 1!! Your turn is over. Its your opponents turn to play.")
+  } else {
+    this.turnScore +=rollDice;
+}
+  return rollDice;
+}
+
 //USER-INTERFACE LOGIC
 $(document).ready(function() {
-    $("#rolled1").click(function() {
-      $("#rolled1value").text(genRandom())
-    });
-    $("#rolled2").click(function() {
-      $("#rolled2value").text(genRandom())
- });
+  $("#rolled1").click(function() {
+    $("#rolled1value").text(playerOne.rolledPlay())
+    $("#turnScore1").text(playerOne.turnScore)
+  });
+  $("#rolled2").click(function() {
+    $("#rolled2value").text(playerTwo.rolledPlay())
+    $("#turnScore2").text(playerTwo.turnScore)
+  });
 });
